bba01ef4136dbb99bab99628352db65136e1696c
Exequiel Raineri <exeraineri15@gmail.com>
2025-01-06 14:49:27 -0300

    estructura de paquetes

---
+++ backend/event-point-backend/src/main/java/com/exeraineri/eventpoint/backend/EventPointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exeraineri.eventpoint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPointBackendApplication.class, args);
+	}
+
+}
---
+++ backend/event-point-backend/src/test/java/com/exeraineri/eventpoint/backend/EventPointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exeraineri.eventpoint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
